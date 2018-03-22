9d6b41c88ce5e3ec8fb2ea2272642eac8b524d17
julian_cardozo <julian@pambumedia.com>
2018-03-22 17:02:02 -0300

    commit inicial

---
+++ src/uat1/ArithmeticDemo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uat1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        int result = 1+2;
+        System.out.println(result);
+        
+        System.out.println("Menos uno");
+        result -= 1;
+        System.out.println(result);
+        
+        System.out.println("Por dos");
+        result *= 2;
+        System.out.println(result);
+         
+        System.out.println("Dividido dos");
+        result /= 2;
+        System.out.println(result);
+        
+        System.out.println("Sumas ocho");
+        result += 8;
+        System.out.println(result);
+        
+        System.out.println("Modulo 7");
+        result %= 7;
+        System.out.println(result);
+        
+        
+       /* int a = 5;
+        int i = 3;
+        
+        System.out.println("Ejercicio 2");
+        a += ++i;
+        System.out.println(a);*/
+        
+         System.out.println("Ejercicio 3");
+         
+         int i = 3;
+         i++;
+         System.out.println(i);
+         ++i;
+         System.out.println(i);
+         System.out.println(++i);
+         System.out.println(i++);
+         System.out.println(i);
+         
+         
+         
+         
+         
+         
+        
+    }
+}
---
+++ src/uat1/PruebaAtributos.java
@@ -0,0 +1,63 @@
+package uat1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaAtributos {
+    //public static String nombre = "Nombre";
+    
+     private static String nombre;
+     
+      private static int entero;
+      
+     public static void main(String[] args) {
+         
+         System.out.println(nombre);
+           System.out.println(entero);
+         //String str = nombre;
+         
+         //String $_0123456 = "laskda";
+         
+        /* int sinInicializar;
+         
+         System.out.print(sinInicializar);
+         
+         
+         String stringSinInicializar;
+         
+         System.out.print(stringSinInicializar);*/
+         /*variable sinInicializar might not have been initialized*/
+        //System.out.println(stringSinInicializar); 
+         
+         /*int entero = "STRING";
+         /*incompatible types: String cannot be converted to int*/
+         
+         //String cadena = 0.0;
+         /*incompatible types: double cannot be converted to String*/
+         
+         //Long along = 1234L;
+         //Long along = 1234F;
+         /*incompatible types: float cannot be converted to Long*/
+         
+         //Float afloat = 1234F;
+         
+        // String colorAuto = "Color auto";
+         
+        // String 5cadenas = "slksjdf";
+         /*not a statement ';' expected*/
+         
+         
+         
+         
+
+         
+        
+     }
+}
